923cef52380e17deec56b0f9e3cd81ae1f0b741d
nghiatv <nghiait0111@gmail.com>
2022-05-04 16:24:12 +0700

    bump version to 1.0.5

--- bin/create-nano-module.ts
+++ bin/create-nano-module.ts
@@ -1,65 +1,65 @@
 #!/usr/bin/env node
-import inquirer from 'inquirer'
-import shell from 'shelljs'
-import fs from 'fs'
-import path from 'path'
-import { buildProject } from '../src/index'
-import { Project } from '../src/types'
+import inquirer from "inquirer"
+import shell from "shelljs"
+import fs from "fs"
+import path from "path"
+import { buildProject } from "../src/index"
+import { Project } from "../src/types"
 ;(async function () {
   const answers = await inquirer.prompt<Project>([
     {
-      type: 'input',
-      message: 'Pick the name of your app:',
-      name: 'name',
-      default: 'module'
+      type: "input",
+      message: "Pick the name of your app:",
+      name: "name",
+      default: "module"
     },
     {
-      type: 'list',
-      message: 'Project Type:',
-      name: 'type',
-      choices: ['Application'],
-      default: 'Application'
+      type: "list",
+      message: "Project Type:",
+      name: "type",
+      choices: ["Application"],
+      default: "Application"
     }
   ])
 
-  if (answers.type === 'Application') {
+  if (answers.type === "Application") {
     const templates = fs
-      .readdirSync(path.join(__dirname, '../templates/application'))
+      .readdirSync(path.join(__dirname, "../templates/application"))
       .sort()
 
     const appAnswers = await inquirer.prompt<Project>([
       {
-        type: 'input',
-        message: 'Port number:',
-        name: 'port',
-        default: '3030'
+        type: "input",
+        message: "Port number:",
+        name: "port",
+        default: "3030"
       },
       {
-        type: 'list',
-        message: 'Framework:',
-        name: 'framework',
+        type: "list",
+        message: "Framework:",
+        name: "framework",
         choices: templates,
-        default: 'react'
+        default: "react"
       },
       {
-        type: 'list',
-        message: 'Language:',
-        name: 'language',
-        choices: ['typescript'],
-        default: 'typescript'
+        type: "list",
+        message: "Language:",
+        name: "language",
+        choices: ["typescript"],
+        default: "typescript"
       },
       {
-        type: 'list',
-        message: 'CSS:',
-        name: 'css',
-        choices: ['CSS', 'Tailwind'],
-        default: 'Tailwind'
+        type: "list",
+        message: "CSS:",
+        name: "css",
+        choices: ["CSS", "Tailwind"],
+        default: "Tailwind"
       },
       {
-        type: 'input',
+        type: "input",
         message: "Tailwind Prefix:  (ignore if you don't use Tailwind)",
-        name: 'prefix',
-        default: ''
+        name: "prefix",
+        default: ""
       }
     ])
 
