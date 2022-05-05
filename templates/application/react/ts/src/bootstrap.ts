7ca0767f933c1be1742b398341e797c5e02daf71
nghiatv <nghiait0111@gmail.com>
2022-05-05 10:32:58 +0700

    fix import css in ts template

--- templates/application/react/ts/src/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-import { LocalDev } from './App'
-import ReactDOM from 'react-dom'
-
-ReactDOM.render(<LocalDev />, document.getElementById('app'))
---
+++ templates/application/react/ts/src/bootstrap.tsx
@@ -0,0 +1,4 @@
+import { LocalDev } from "./App"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<LocalDev />, document.getElementById("app"))
